8b17858d9b81a6a6817ed65ca73fa4f9775e14fa
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-04-27 19:33:09 +0200

    addesd setters and getters to alpha property

--- Ramie_Robota/ShoulderJoint.h
+++ Ramie_Robota/ShoulderJoint.h
@@ -10,5 +10,7 @@ public:
     ShoulderJoint(const JointConnector&, const Coordinates&);
     float rotate(const int);
     float rotate_one_degree();
+    int get_alpha()const;
+    void set_alpha(const int);
 };
 
