374eccef5c010b42622e7242b21a78293b94b96a
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-04-27 19:29:18 +0200

    added steres and getters

--- Ramie_Robota/ElbowJoint.cpp
+++ Ramie_Robota/ElbowJoint.cpp
@@ -6,3 +6,13 @@ ElbowJoint::ElbowJoint(const JointConnector& previous_joint_connector, const Joi
 	this->my_previous_joint_connector = previous_joint_connector;
 	coordinates = previous_joint_connector.get_end_coordinates();
 }
+
+JointConnector ElbowJoint::get_previous_joint_connector() const
+{
+	return my_previous_joint_connector;
+}
+
+void ElbowJoint::set_previous_joint_connector(const JointConnector& previous_joint_connector)
+{
+	my_previous_joint_connector = previous_joint_connector;
+}
