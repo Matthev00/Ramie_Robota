1b66cd6941ebfa8a2a91dcd4e89db0769909ee57
Krzysztof Gólcz <01178525@pw.edu.pl>
2023-05-06 20:43:44 +0200

    Added min_x max_x

--- Ramie_Robota/JointConnector.h
+++ Ramie_Robota/JointConnector.h
@@ -1,7 +1,6 @@
 #pragma once
 //Author: Krzysztof Gólcz
 #include "coordinates.h"
-#include "Joint.h"
 #include <math.h>
 #include "Wektor.h"
 
@@ -11,13 +10,10 @@ private:
 	Coordinates begin_coordinates;
 	Coordinates end_coordinates;
 	Wektor direction;
-	
+	float tg_angle;
 public:
-	JointConnector(Coordinates, Coordinates);
-	
-	float count_length()const;
+	JointConnector(Coordinates, Coordinates, float);
 
-	
 	Coordinates get_begin_coordinates()const;
 	void set_begin_coordinates(const Coordinates);
 	Coordinates get_end_coordinates()const;
@@ -29,6 +25,8 @@ public:
 
 	void update_end_coordinates();
 	float get_lenght()const;
+
 	float max_x()const;
+	float min_x()const;
 
 };
