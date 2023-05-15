ddb1c0e7767aabf3e2ff3e2806aaa05d78b85235
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-05-15 17:06:08 +0200

    expanded arm class to do: reach target

--- Ramie_Robota/Arm.cpp
+++ Ramie_Robota/Arm.cpp
@@ -7,6 +7,80 @@ Arm::Arm()
 {
 }
 
+Arm::Arm(JointConnector& part_1, JointConnector& part_2, Coordinates coords)
+{
+	this->arm_part = part_1;
+	this->forearm = part_2;
+	ShoulderJoint sh(arm_part, coords);
+	shoulder = sh;
+	ElbowJoint el(arm_part, forearm);
+	elbow = el;
+	Gripper g(forearm.get_end_coordinates());
+	gripper = g;
+	/// max lenght of elbow
+	range = arm_part.get_lenght() + forearm.get_lenght() + gripper.get_range();
+	tg = 0.0f;
+	lenght = arm_part.get_lenght() + forearm.get_lenght();
+}
+
+void Arm::uptade()
+{
+	elbow.set_coordinates(arm_part.get_end_coordinates());
+	
+	float x_end = cout_end_forearm_x();
+	float x = x_end - start_coords.x;
+	float y = tg * x - start_coords.y;
+	float old_z = forearm.get_end_coordinates().z;
+	float new_z = sqrt(lenght * lenght - x * x - y * y);
+	float begin_z = start_coords.z;
+	float z = (begin_z + new_z < old_z) ? begin_z + new_z : begin_z - new_z;
+	Coordinates coords(
+		x + start_coords.x,
+		y + start_coords.y,
+		z);
+	forearm.set_end_coordinates(coords);
+
+	gripper.set_coordinates(coords);
+}
+
+bool Arm::reach_target(Coordinates& target)
+{
+	float target_distance = sqrt(
+		pow(target.x - start_coords.x, 2) +
+		pow(target.y - start_coords.y, 2) +
+		pow(target.z - start_coords.z, 2));
+
+	if (target_distance > range) {
+		return false;
+	}
+
+	float elbow_lenght = arm_part.get_lenght();
+
+	if (target_distance > elbow_lenght) {
+		/*
+		Dostosuj d³ugoœæ forearm
+		zegij bark troche
+		rotuj
+		zegnij bark
+		chwyæ
+		*/
+	}
+	else {
+		/*
+		zegij bark troche
+		rotuj
+		zegnij bark
+		zegnij ³okieæ
+		dostosuj d³ugoœæ
+		chwyæ
+		*/
+	}
+
+
+
+	return false;
+}
+
 bool Arm::if_reachable(double x, double y, double z) const
 {
 	double dst = sqrt(x * x + y * y + z * z);
@@ -16,15 +90,15 @@ bool Arm::if_reachable(double x, double y, double z) const
 	return false;
 }
 
-double Arm::get_range() const
+float Arm::get_range() const
 {
 	return range;
 }
 
-void Arm::set_range(double new_range)
+float Arm::cout_end_forearm_x() const
 {
-	if (new_range < 0) {
-		throw "Range can not be negative.";
-	}
-	range = new_range;
+	float len = arm_part.get_lenght() + forearm.get_lenght();
+	float y = arm_part.get_lenght();
+	float x = arm_part.get_end_coordinates().x;
+	return len * x / y;
 }
--- Ramie_Robota/Arm.h
+++ Ramie_Robota/Arm.h
@@ -1,11 +1,29 @@
 #pragma once
+#include "Joint.h"
+#include "ElbowJoint.h"
+#include "ShoulderJoint.h"
+#include "coordinates.h"
+#include "Gripper.h"
 class Arm
 {
 private:
-	double range{};
+	Coordinates start_coords;
+	ShoulderJoint shoulder;
+	ElbowJoint elbow;
+	JointConnector arm_part;
+	JointConnector forearm;
+	Gripper gripper;
+	float range;
+	float lenght;
+	float tg;
+
 public:
 	Arm();
+	Arm(JointConnector&, JointConnector&, Coordinates = Coordinates());
+	void uptade();
+	bool reach_target(Coordinates&);
 	bool if_reachable(double, double, double) const;
-	double get_range() const;
-	void set_range(double);
+	float get_range() const;
+	float get_tg() const;
+	float cout_end_forearm_x()const;
 };
