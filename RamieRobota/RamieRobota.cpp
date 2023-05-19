c3f8efcb8b343655e125847b995ed3aafcc46141
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-05-20 00:43:02 +0200

    example of usage in main

--- RamieRobota/RamieRobota.cpp
+++ RamieRobota/RamieRobota.cpp
@@ -3,10 +3,23 @@
 #include "Arm.h"
 #include <iostream>
 
-int main()
-{
-    Arm arm;
-    arm.set_range(10.1);
-    std::cout << arm.if_reachable(1, 1, 1) << "\n";
+
+
+int main() {
+    JointConnector arm_part(Coordinates(0, 0, 0), Coordinates(0, 0, 10));
+    ExtandebleJointConnector forearm(Coordinates(0, 0, 10), Coordinates(0, 0, 20));
+    Arm arm(arm_part, forearm);
+    Coordinates target(-5, -5, 5);
+    std::vector < std::vector<Coordinates>>result = arm.reach_target(target);
+
+
+    for (int i = 0; i < result[0].size();i++) {
+        // i-ty koordynat barku
+        std::cout << result[0][i];
+        // i-ty koordynat łokcia
+        std::cout << result[1][i];
+        // i-ty koordynat chwytaka
+        std::cout << result[2][i] <<  std::endl;
+    }
 
 }
