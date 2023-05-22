83e98dec138639564956a9dd16bf782867ebd842
Krzysztof Gólcz <01178525@pw.edu.pl>
2023-05-22 20:10:52 +0200

    Added going back to a certain step

--- RamieRobota/RamieRobota.cpp
+++ RamieRobota/RamieRobota.cpp
@@ -12,11 +12,13 @@ float cameraX = 1.0f;
 float cameraY = 1.0f;
 float cameraZ = 5.0f;
 std::vector < std::vector<Coordinates>>result;
+Coordinates target(2, 5, 5);
 int i;
 const int SCALE = 10.0f;
 
 
 void display() {
+    
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -64,6 +66,10 @@ void display() {
     glBegin(GL_POINTS);
     glVertex3d(result[1][i].x / SCALE, result[1][i].y / SCALE, result[1][i].z / SCALE);
     glVertex3d(result[2][i].x / SCALE, result[2][i].y / SCALE, result[2][i].z / SCALE);
+
+    glColor3f(0.0f, 0.2f, 1.0f);
+    glVertex3d(target.x / SCALE, target.y / SCALE, target.z / SCALE);
+
     glEnd();
 
     glFlush();
@@ -91,6 +97,14 @@ void specialKeys(int key, int x, int y) {
     case GLUT_KEY_F2:
         cameraY -= cameraSpeed;
         break;
+    case GLUT_KEY_F6:
+        if (i > 0)
+            i--;
+        break;
+    case GLUT_KEY_F7:
+        if (i < result[0].size() - 1)
+            i++;
+        break;
     }
 
     glutPostRedisplay();
@@ -104,30 +118,21 @@ int main(int argc, char** argv) {
     JointConnector arm_part(Coordinates(0, 0, 0), Coordinates(0, 0, 10));
     ExtandebleJointConnector forearm(Coordinates(0, 0, 10), Coordinates(0, 0, 20));
     Arm arm(arm_part, forearm);
-    Coordinates target(2, 5, 5);
+    std::cout << "What point do you want to reach (x, y, z)" << std::endl;
+    std::cin >> target;
+
+    
     result = arm.reach_target(target);
     i = result[1].size() - 1;
 
-    for (int i = 0; i < result[0].size();i++) {
+    for (int j = 0; j < result[0].size();j++) {
         // i-ty koordynat barku
-        std::cout << result[0][i];
+        std::cout << result[0][j];
         // i-ty koordynat łokcia
-        std::cout << result[1][i];
+        std::cout << result[1][j];
         // i-ty koordynat chwytaka
-        std::cout << result[2][i] <<  std::endl;
+        std::cout << result[2][j] <<  std::endl;
     }
-    /*
-    result = arm.back_to_starting_pos();
-    
-    for (int i = 0; i < result[0].size(); i++) {
-        // i-ty koordynat barku
-        std::cout << result[0][i];
-        // i-ty koordynat łokcia
-        std::cout << result[1][i];
-        // i-ty koordynat chwytaka
-        std::cout << result[2][i] << std::endl;
-    }
-    */
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
@@ -140,7 +145,7 @@ int main(int argc, char** argv) {
     glutSpecialFunc(specialKeys);
 
     glutMainLoop();
-
+    
     
     return 0;
 
